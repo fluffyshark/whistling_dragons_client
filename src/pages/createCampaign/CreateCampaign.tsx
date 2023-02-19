72c7a60a132f8055001a2838959aaa08644aa087
fluffyshark <robin.yjord1@gmail.com>
2023-02-19 16:24:22 +0100

    Structure - Componentized the creation of campaign cards (5min)

--- src/pages/createCampaign/CreateCampaign.tsx
+++ src/pages/createCampaign/CreateCampaign.tsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
 import sunCrownImg from "../../components/assets/misc/img_sun_crown.png"
-import icon_upload_image from "../../components/assets/misc/icon_upload_image.png"
+import CreateCampaignCard from './components/CreateCampaignCard'
+
+
 
 type Props = {}
 
 const CreateCampaign = (props: Props) => {
 
-    const [textValue, setTextValue] = useState<string>('');
-
-    const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setTextValue(event.target.value);
-      };
+   
 
   return (
     <div className='createCampaign'>
@@ -29,28 +27,8 @@ const CreateCampaign = (props: Props) => {
         </div>
 
 
-        <div className="createCampaign_cardContainer">
-
-            <div className="createCampaign_cardContainer_card">
-                <div className="createCampaign_cardContainer_card_imageContainer">
-                    <img src={icon_upload_image} alt="" />    
-                </div> 
-                <div className="createCampaign_cardContainer_card_descriptions">
-                    <div className="createCampaign_cardContainer_card_descriptions_title">
-                        <input type="text" placeholder='Title...' />
-                    </div>
-                    <div className="createCampaign_cardContainer_card_descriptions_text">
-                        <textarea placeholder='Description...' value={textValue} onChange={handleTextChange} name="text" id="text" cols={1} rows={2} />
-                    </div>
-                    <div className="createCampaign_cardContainer_card_descriptions_players">
-                        <input type="text" placeholder='How many player...' />
-                    </div>
-                </div>
-            </div>
-          
-            <button className="createCampaign_cardContainer_cardNextBtn"><p>Next</p></button>
-
-        </div>
+        <CreateCampaignCard />
+        
 
     </div>
   )
