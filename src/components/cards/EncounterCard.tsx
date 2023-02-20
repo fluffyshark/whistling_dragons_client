4bcce7cf781d83d8bdc7d20a55ba436a415d4182
fluffyshark <robin.yjord1@gmail.com>
2023-02-20 04:28:07 +0100

    Design - Adjusted the editable encounter card (30min)

--- src/components/cards/EncounterCard.tsx
+++ src/components/cards/EncounterCard.tsx
@@ -20,11 +20,21 @@ const CampaignCard = ({scale}: CampaignCard) => {
     )}
  
     {scale === "normal" && (
-        <div className="encounterCard">
-            <div className="encounterCard_imageContainer"></div> 
-            <div className="encounterCard_descriptions">
-                <div className="encounterCard_descriptions_title"><p>Resolute Soilder</p><p>Monster</p></div>
-                <div className="encounterCard_descriptions_text"><p>This adventure will bring our heroes to a mysterious land. How anyone ended up there is unknown, even to them. Still our heroes must travel will travel there vast lands to discover their way home. But will they survive until then...</p></div>
+        <div className="encounterCard_normal">
+            <div className="encounterCard_normal_imageContainer"></div> 
+            <div className="encounterCard_normal_descriptions">
+                <div className="encounterCard_normal_descriptions_title"><p>Resolute Soilder</p><p>Monster</p></div>
+                <div className="encounterCard_normal_descriptions_text"><p>This adventure will bring our heroes to a mysterious land. How anyone ended up there is unknown, even to them. Still our heroes must travel will travel there vast lands to discover their way home. But will they survive until then...</p></div>
+            </div>
+        </div>
+    )}
+
+    {scale === "edit" && (
+        <div className="encounterCard_edit">
+            <div className="encounterCard_edit_imageContainer"></div> 
+            <div className="encounterCard_edit_descriptions">
+                <div className="encounterCard_edit_descriptions_title"><p>Resolute Soilder</p><p>Monster</p></div>
+                <div className="encounterCard_edit_descriptions_text"><p>This adventure will bring our heroes to a mysterious land. How anyone ended up there is unknown, even to them. Still our heroes must travel will travel there vast lands to discover their way home. But will they survive until then...</p></div>
             </div>
         </div>
     )}
--- src/pages/createCampaign/components/CardCreator.tsx
+++ src/pages/createCampaign/components/CardCreator.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import icon_upload_image from "../../../components/assets/misc/icon_upload_image.png"
+import icon_sword from "../../../components/assets/misc/icon_sword.png"
+import icon_heart from "../../../components/assets/misc/icon_heart.png"
+import icon_defence from "../../../components/assets/misc/icon_armor.png"
 
 type CardCreator = {
     cardType:string
@@ -39,19 +42,35 @@ const CardCreator = ({cardType}: CardCreator) => {
 
         {cardType === "encounter" && (
 
-            <div className="cardCreator">
-                <div className="cardCreator_imageContainer">
+            <div className="cardCreator_encounter">
+                <div className="cardCreator_encounter_imageContainer">
                     <img src={icon_upload_image} alt="" />    
                 </div> 
-                <div className="cardCreator_descriptions">
-                    <div className="cardCreator_descriptions_title">
+                <div className="cardCreator_encounter_descriptions">
+                    <div className="cardCreator_encounter_descriptions_title">
                         <input type="text" placeholder='Title...' />
                     </div>
-                    <div className="cardCreator_descriptions_text">
+                    <div className="cardCreator_encounter_descriptions_type">
+                        <p>Environment</p>
+                        <p>Monster</p>
+                        <p>Quest</p>
+                    </div>
+                    <div className="cardCreator_encounter_descriptions_text">
                         <textarea placeholder='Description...' value={textValue} onChange={handleTextChange} name="text" id="text" cols={1} rows={2} />
                     </div>
-                    <div className="cardCreator_descriptions_players">
-                        <input type="text" placeholder='How many player...' />
+                    <div className="cardCreator_encounter_descriptions_stats">
+                        <div className="cardCreator_encounter_descriptions_stats_field">
+                            <img src={icon_sword} alt="" />
+                            <input type="text" placeholder='10' />
+                        </div>
+                        <div className="cardCreator_encounter_descriptions_stats_field">
+                            <img src={icon_heart} alt="" />
+                            <input type="text" placeholder='10' />
+                        </div>
+                        <div className="cardCreator_encounter_descriptions_stats_field">
+                            <img src={icon_defence} alt="" />
+                            <input type="text" placeholder='10' />
+                        </div>
                     </div>
                 </div>
             </div>
